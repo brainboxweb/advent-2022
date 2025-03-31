9b52362e776ac5ba40e59787d78c7049063f2a62
Gary Straughan <gary.straughan@brainboxweb.co.uk>
2025-03-31 09:42:03 +0100

    Day 3

---
+++ day3/day3_test.go
@@ -0,0 +1,57 @@
+package day3_test
+
+import (
+	"testing"
+
+	"github.com/brainboxweb/advent-2022/day3"
+	"github.com/brainboxweb/advent-2022/helpers"
+	"github.com/stretchr/testify/assert"
+)
+
+const dataPath = "../data/"
+
+func TestDay3(t *testing.T) {
+	tests := []struct {
+		dataFile string
+		expected int
+	}{
+		{
+			dataPath + "day3_test.txt",
+			157,
+		},
+		{
+			dataPath + "day3.txt",
+			7848,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.dataFile, func(t *testing.T) {
+			dataSet := helpers.GetDataString(tt.dataFile)
+			result := day3.Part1(dataSet)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestDay3_2(t *testing.T) {
+	tests := []struct {
+		dataFile string
+		expected int
+	}{
+		{
+			dataPath + "day3_test.txt",
+			70,
+		},
+		{
+			dataPath + "day3.txt",
+			2616,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.dataFile, func(t *testing.T) {
+			dataSet := helpers.GetDataString(tt.dataFile)
+			result := day3.Part2(dataSet)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
